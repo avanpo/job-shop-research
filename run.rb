0cc82375e7d7aa5c5ec0a7a3c047942497ce66e7
Alex van Poppelen <avanpoppelen@gmail.com>
2016-01-25 06:05:56 +0100

    updated paper, run script

--- run.rb
+++ run.rb
@@ -11,20 +11,22 @@ runs = ARGV[1].to_i
 s = Array.new
 t = Array.new
 
+runs.times do |i|
+  output = `#{command}`
+  s << output.match(/found: ([0-9]+)/).captures[0].to_i
+  t << output.match(/time: ([0-9]+)/).captures[0].to_i
+end
+
 File.open("out.txt", "a") do |dest|
   dest.puts "Command: #{command}"
   dest.puts "Runs: #{runs}\n"
   dest.puts
-  runs.times do |i|
-    output = `#{command}`
-    s << output.match(/found: ([0-9]+)/).captures[0].to_i
-    t << output.match(/time: ([0-9]+)/).captures[0].to_i
+  0.upto(s.size - 1) do |i|
     dest.puts "#{s[i]}   #{t[i]}"
   end
   s_bar = s.inject(:+).to_f / runs
   t_bar = t.inject(:+).to_f / runs
   dest.puts "-----------"
   dest.puts "#{s_bar.round(1)} #{t_bar.round(1)}"
-  dest.puts
   dest.puts "====================================="
 end
